584cdf8aca9d95266767dec2bdfb81acc36069b6
l1irik-rbk <kir.baranow2010@gmail.com>
2022-06-19 11:30:11 +0300

    fix: delete dist

--- dist/utils/getId.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getId = void 0;
-const getId = (url) => url.split('/')[3];
-exports.getId = getId;
--- dist/utils/uuidValidateV4.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.uuidValidateV4 = void 0;
-const uuid_1 = require("uuid");
-const uuid_2 = require("uuid");
-const uuidValidateV4 = (uuid) => {
-    return (0, uuid_2.validate)(uuid) && (0, uuid_1.version)(uuid) === 4;
-};
-exports.uuidValidateV4 = uuidValidateV4;
